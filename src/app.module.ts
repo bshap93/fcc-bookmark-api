eecb066046c4783262729590289d2ac004b2e0a4
Benjamin Shapiro <bshap93@gmail.com>
2022-09-21 19:57:20 -0600

    My first commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [UserModule, BookmarkModule],
+})
+export class AppModule {}
+
+// 15:43 in video
