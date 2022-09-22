93def016fe2dd0333cc657e4e6f8d260fb62b358
Benjamin Shapiro <bshap93@gmail.com>
2022-09-22 07:27:52 -0600

    Added auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
-
-// 15:43 in video
